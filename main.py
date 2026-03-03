0ae2856587b708e2f8897ccc5ccdf85edae63ff3
Taha-2026 <idrisi.abu.tahaa@gmail.com>
2026-03-03 19:48:15 +0530

    Remove unnecessary print statement and comments

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 # Coder:ABUTAHA IDRISI
 # Date:3/3/26
 
-print("--- Pattern Printer ---")
-
-n = int(input(Enter Rows: ))
+n = int(input())
 for i in range(1, n + 1):
     for j in range(i):
         print("*", end="")
     print()
+
 
 
 
