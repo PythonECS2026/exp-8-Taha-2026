d53cf8a17fa2547804f3af71ad8844270721f8d3
Taha-2026 <idrisi.abu.tahaa@gmail.com>
2026-03-03 19:46:14 +0530

    Fix input prompt for number of rows in pattern

--- main.py
+++ main.py
@@ -6,11 +6,12 @@
 
 print("--- Pattern Printer ---")
 
-n = int(input())
+n = int(input(Enter Rows: ))
 for i in range(1, n + 1):
     for j in range(i):
         print("*", end="")
     print()
+
 
 
 
